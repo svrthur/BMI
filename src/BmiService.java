91439b5cc91bdadb168bae8d490796d29096bdc2
arthur.salahutdinov <artes_93@mail.ru>
2024-09-02 23:05:23 +0300

    Добавлены обновления в папке src

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class BmiService {
+    public int calculate(int weightKg, double heightMeters) {
+        double bmi = weightKg / (heightMeters * heightMeters);
+        return (int) bmi; // Приведение результата к целому числу
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 98; // вес в килограммах
+        double height = 1.87; // рост в метрах
+
+        int bmi = service.calculate(weight, height);
+        System.out.println("Индекс массы тела: " + bmi);
+    }
 }
